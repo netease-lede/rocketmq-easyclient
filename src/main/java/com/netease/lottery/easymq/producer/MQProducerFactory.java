612220ac727e4a44ca75b9ccaf0a3a1d0aad3a74
bjguosong <bjguosong@corp.netease.com>
2016-10-26 21:06:17 +0800

    编译报错修改

--- src/main/java/com/netease/lottery/easymq/producer/MQProducerFactory.java
+++ src/main/java/com/netease/lottery/easymq/producer/MQProducerFactory.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.netease.lottery.easymq.constant.MQConstant;
+import com.netease.lottery.easymq.producer.MQProducer;
 
 public class MQProducerFactory
 {
