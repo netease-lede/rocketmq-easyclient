283763947411e0f8134adc3b0dfc3c8dc0e67ac3
bjchenyuan <bjchenyuan@corp.netease.com>
2017-01-17 19:25:44 +0800

    producer添加shutdown

--- easymq-example/src/main/java/com/netease/lottery/easymq/example/producer/TestProducer.java
+++ easymq-example/src/main/java/com/netease/lottery/easymq/example/producer/TestProducer.java
@@ -5,29 +5,32 @@ import com.netease.lottery.easymq.common.exception.MqWapperException;
 import com.netease.lottery.easymq.producer.EasyMQProducer;
 import com.netease.lottery.easymq.producer.EasyMQProducerFactory;
 
-public class TestProducer
-{
-	public static void main(String[] args)
-	{
+public class TestProducer {
+	public static void main(String[] args) {
 		EasyMQProducer producer = EasyMQProducerFactory.getProducer();
-		try
-		{
+		try {
 			//producer.sendMsg("topic20161116", "order10", "order10detail");
 			System.out.println("begin");
 			long begin = System.currentTimeMillis();
-			for (int i = 10; i <= 14; i++)
-			{
+			for (int i = 10; i <= 14; i++) {
 				producer.sendMsgOrderly("topic20161119", i + "", i + "detail", "onlyyou");
 			}
 			System.out.println("send,taking" + (System.currentTimeMillis() - begin) + "ms");
-		}
-		catch (MqWapperException e)
-		{
+		} catch (MqWapperException e) {
+			e.printStackTrace();
+		} catch (MqBussinessException e) {
 			e.printStackTrace();
 		}
-		catch (MqBussinessException e)
-		{
-			e.printStackTrace();
-		}
+
+		/** 
+		 * 应用退出时，要调用shutdown来清理资源，关闭网络连接，从MetaQ服务器上注销自己 
+		 * 注意：我们建议应用在JBOSS、Tomcat等容器的退出钩子里调用shutdown方法 
+		 */
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			public void run() {
+				producer.shutdown();
+			}
+		}));
+		System.exit(0);
 	}
 }
