9bc911e8d5f6844bc86aa7c4d4977a23faf5b7c4
mengchenfei <Mm196225>
2017-02-20 21:35:27 +0800

    [修改包名]

---
+++ easyclient-example/src/main/java/com/lede/tech/rocketmq/easyclient/example/consumer/TestMQHandler1.java
@@ -0,0 +1,38 @@
+package com.lede.tech.rocketmq.easyclient.example.consumer;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.lede.tech.rocketmq.easyclient.consumer.bean.MessageBean;
+import com.lede.tech.rocketmq.easyclient.consumer.handler.EasyMQRecMsgHandler;
+import com.lede.tech.rocketmq.easyclient.consumer.handler.annotation.EasyMQConsumerMeta;
+
+/**
+ * 
+ * @Desc 
+ * @Author ykhu
+ */
+@EasyMQConsumerMeta(topic = "topic20170119", group = "group1", isOrderly = true)
+public class TestMQHandler1 implements EasyMQRecMsgHandler
+{
+	private volatile static AtomicInteger count = new AtomicInteger(0);
+
+	public void handle(List<MessageBean> msg) throws Exception
+	{
+
+		for (MessageBean message : msg)
+		{
+			int nowCount = count.incrementAndGet();
+			int keyInt = Integer.parseInt(message.getKey());
+			System.out.println("group1 PaySuccessMQHandler" + message);
+			System.out.println("group1 key:" + message.getKey());
+			System.out.println("group1 message:" + message.getMessage());
+			//			if (keyInt % 3 == 0)
+			//			{
+			//				System.out.println(new Date());
+			//				throw new RuntimeException();
+			//			}
+		}
+
+	}
+}
